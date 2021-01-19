8540e62948ca976b3ba1472fae30b26c9d0032a8
catlair <catlair@qq.com>
2021-01-19 17:10:14 +0800

    消息推送页面的表单验证

--- src/views/BaseMessagePush.vue
+++ src/views/BaseMessagePush.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="base-message-push">
-    <el-form ref="form" :model="message" label-width="140px">
+    <el-form ref="emailForm" :model="message.email" :rules="rules" label-width="140px">
       <el-form-item label="使用邮箱推送">
         <el-switch v-model="messagePush.useEmail"></el-switch>
       </el-form-item>
@@ -8,25 +8,27 @@
         <el-input type="email" placeholder="from email"
                   v-model="message.email.from"></el-input>
       </el-form-item>
-      <el-form-item required label="邮箱授权码">
+      <el-form-item label="邮箱授权码" prop="pass">
         <el-input placeholder="邮箱授权码" v-model="message.email.pass"
                   show-password></el-input>
       </el-form-item>
-      <el-form-item label="接收邮箱">
+      <el-form-item label="接收邮箱" prop="to">
         <el-input type="email" placeholder="to email"
                   v-model="message.email.to"></el-input>
       </el-form-item>
-      <el-form-item required label="服务器地址">
+      <el-form-item label="服务器地址" prop="host">
         <el-input placeholder="发送邮件的服务器地址" v-model="message.email.host"></el-input>
       </el-form-item>
-      <el-form-item required label="端口">
-        <el-input placeholder="安全信息默认465" v-model="message.email.port"></el-input>
+      <el-form-item label="端口" prop="port">
+        <el-input placeholder="安全信息默认465" v-model.number="message.email.port"></el-input>
       </el-form-item>
-      <el-divider></el-divider>
+    </el-form>
+    <el-divider></el-divider>
+    <el-form ref="serverChanForm" :model="message" :rules="rules" label-width="140px">
       <el-form-item label="使用server酱">
         <el-switch v-model="messagePush.useServerChan"></el-switch>
       </el-form-item>
-      <el-form-item label="server酱">
+      <el-form-item label="server酱" prop="serverChan">
         <el-input
             type="textarea"
             autosize
@@ -46,6 +48,7 @@ export default {
   name: "BaseMessagePush",
   data() {
     return {
+      nullRules: {},
       timer: null,
       messagePush: {
         useEmail: false,
@@ -60,14 +63,39 @@ export default {
           port: 465
         },
         serverChan: ''
+      },
+      rules: {
+        from: [{required: true, type: 'email', message: '邮箱格式不正确', trigger: 'blur'}],
+        pass: [{required: true, message: '授权码不能为空', trigger: 'blur'}],
+        host: [{required: true, message: '服务器地址复制下呗', trigger: 'blur'}],
+        to: [{type: 'email', message: '邮箱格式不正确', trigger: 'blur'}],
+        port: [{type: 'number', message: '端口为数字', trigger: 'blur'}],
+        serverChan: [{required: true, message: '密钥不能为空', trigger: 'blur'}],
       }
     }
   },
   methods: {
-    onSubmit() {
-      this.$store.commit("updateMessage",this.message);
+    async onSubmit() {
       this.saveDate();
-      this.$router.push('/users')
+      this.$store.commit("updateMessage", this.message);
+
+      const emailValid = await this.validateForm('useEmail', 'emailForm', 'email表单未完成');
+      const serverChanValid = await this.validateForm('useServerChan', 'serverChanForm', 'serverChan表单未完成');
+      if (emailValid && serverChanValid) {
+        this.$message.success('消息配置成功')
+        await this.$router.push('/users')
+      }
+    },
+    validateForm(useName, formName, warning) {
+      if (!this.messagePush[useName]) return true;
+      let validTemp = true;
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          this.$message.warning(warning)
+          return validTemp = false;
+        }
+      });
+      return validTemp;
     },
     saveDate() {
       window.localStorage.setItem('globalConfig', JSON.stringify({
@@ -98,7 +126,9 @@ export default {
 </script>
 
 <style>
-.base-message-push input, .base-message-push textarea {
+.base-message-push input,
+.base-message-push .el-input--suffix,
+.base-message-push textarea {
   width: 300px;
 }
 
