287e10fd429b92dd24d9e11f8a25cb41254494fb
catlair <catlair@qq.com>
2021-01-24 21:53:06 +0800

    修复一些小问题

--- src/utils/form2data.js
+++ src/utils/form2data.js
@@ -1,23 +1,24 @@
-import functionData from "@/data/functionData";
+import functionData from '@/data/functionData';
 
 function getFunctionsOption(closeFunValues) {
-    closeFunValues.forEach(name => {
-        functionData[name] = false
-    })
-    return functionData;
+  const tempFunctions = Object.copy(functionData);
+  closeFunValues.forEach((name) => {
+    tempFunctions[name] = false;
+  });
+  return tempFunctions;
 }
 
 function getSlsOption(scf, control) {
-    if (!control) return null;
-    return scf;
+  if (!control) return null;
+  return scf;
 }
 
 function getDailyRunTime(dailyRunTime) {
-    return dailyRunTime[0] + '-' + dailyRunTime[1];
+  return dailyRunTime[0] + '-' + dailyRunTime[1];
 }
 
 function getCustomizeUp(customizeUp) {
-    return customizeUp.map(up => up.value).filter(up => up !== '');
+  return customizeUp.map((up) => up.value).filter((up) => up !== '');
 }
 
 function getAccountMessage(account) {
@@ -29,48 +30,48 @@ function getAccountMessage(account) {
 
   const tempMessage = {};
 
-  Object.keys(message).forEach(el => {
+  Object.keys(message).forEach((el) => {
     if (!control[el].use) return;
     if (control[el].custom) {
       tempMessage[el] = message[el];
       return;
     }
     tempMessage[el] = true;
-  })
+  });
 
   return tempMessage;
 }
 
 function getAccountOne(account) {
-    const {baseConfig, coinsConfig, closeFunValues, slsConfig} = account;
-    return {
-        function: getFunctionsOption(closeFunValues),
-        dailyRunTime: getDailyRunTime(slsConfig.dailyRunTime),
-        ...baseConfig,
-        message: getAccountMessage(account),
-        ...coinsConfig,
-        customizeUp: getCustomizeUp(coinsConfig.customizeUp),
-        sls: getSlsOption(slsConfig.scf, account.switch.scf)
-    };
+  const { baseConfig, coinsConfig, closeFunValues, slsConfig } = account;
+  return {
+    function: getFunctionsOption(closeFunValues),
+    dailyRunTime: getDailyRunTime(slsConfig.dailyRunTime),
+    ...baseConfig,
+    message: getAccountMessage(account),
+    ...coinsConfig,
+    customizeUp: getCustomizeUp(coinsConfig.customizeUp),
+    sls: getSlsOption(slsConfig.scf, account.switch.scf),
+  };
 }
 
 function getMessageData(message) {
   if (Object.isEmpty(message)) return null;
-    const {form, switch: control} = message,
-        tempMessage = {};
-    Object.keys(control).forEach(el => {
-        control[el] && (tempMessage[el] = form[el]);
-    })
-    return tempMessage;
+  const { form, switch: control } = message,
+    tempMessage = {};
+  Object.keys(control).forEach((el) => {
+    control[el] && (tempMessage[el] = form[el]);
+  });
+  return tempMessage;
 }
 
 function getAccountData(account) {
-    return Object.keys(account).map(key => getAccountOne(account[key]))
+  return Object.keys(account).map((key) => getAccountOne(account[key]));
 }
 
-export default function (account, message) {
-    return Object.copy({
-        message: getMessageData(message),
-        account: getAccountData(account)
-    })
+export default function(account, message) {
+  return Object.copy({
+    message: getMessageData(message),
+    account: getAccountData(account),
+  });
 }
--- src/views/DataFormat.vue
+++ src/views/DataFormat.vue
@@ -3,22 +3,31 @@
     <el-col :span="12">
       <div class="grid-content bg-purple">
         输入gzip加密数据
-        <el-button type="primary" @click="getGzip" style="margin: 0 0 6px 46%;">从表单获取</el-button>
+        <el-button type="primary" @click="getGzip" style="margin: 0 0 6px 46%;"
+          >从表单获取</el-button
+        >
         <el-input
-            type="textarea"
-            :autosize="{minRows: 30}"
-            v-model="gzipText">
+          type="textarea"
+          :autosize="{ minRows: 30 }"
+          v-model="gzipText"
+        >
         </el-input>
       </div>
     </el-col>
     <el-col :span="12">
       <div class="grid-content bg-purple-light">
         输入json数据
-        <el-button type="primary" style="margin: 0 0 6px 46%;" @click="restoreForm">还原设置到表单</el-button>
+        <el-button
+          type="primary"
+          style="margin: 0 0 6px 46%;"
+          @click="restoreForm"
+          >还原设置到表单</el-button
+        >
         <el-input
-            type="textarea"
-            :autosize="{minRows: 20,maxRows:30}"
-            v-model="jsonText">
+          type="textarea"
+          :autosize="{ minRows: 20, maxRows: 30 }"
+          v-model="jsonText"
+        >
         </el-input>
       </div>
     </el-col>
@@ -26,68 +35,76 @@
 </template>
 
 <script>
-import data2form from "@/utils/data2form";
-import gzip from "@/utils/gzip";
-import form2data from "@/utils/form2data";
+import data2form from '@/utils/data2form';
+import gzip from '@/utils/gzip';
+import form2data from '@/utils/form2data';
 
 export default {
-  name: "DataFormat",
+  name: 'DataFormat',
   data() {
     return {
       json: '',
       gzip: '',
-    }
+    };
   },
   computed: {
     gzipText: {
       get() {
-        return this.gzip
+        return this.gzip;
       },
       set(value) {
-        this.gzip = value
-        this.json = this.gzip2json(value)
-      }
+        this.gzip = value;
+        this.gzip2json(value);
+      },
     },
     jsonText: {
       get() {
-        return this.json
+        return this.json;
       },
       set(value) {
-        this.json = value
+        this.json = value;
         if (value === '') {
-          this.gzip = ''
-          return
+          this.gzip = '';
+          return;
         }
-        this.gzip = gzip.gzipEncode(value)
-      }
-    }
+        //string,parse一下是去掉空格,让数据
+        try {
+          this.gzip = gzip.gzipEncode(JSON.stringify(JSON.parse(value)));
+        } catch (error) {
+          this.gzip = '非正确json';
+        }
+      },
+    },
   },
   methods: {
     gzip2json(value) {
-      this.json = JSON.stringify(JSON.parse(gzip.gzipDecode(value)), null, 3)
+      try {
+        this.json = JSON.stringify(JSON.parse(gzip.gzipDecode(value)), null, 3);
+      } catch (error) {
+        //无效
+        this.json = '非json数据';
+      }
     },
     restoreForm() {
       if (!this.json) return;
-      const data = data2form(JSON.parse(this.json))
-      this.$message.success('还原成功')
-      this.$store.commit('restoreForm', data)
+      const data = data2form(JSON.parse(this.json));
+      this.$message.success('还原成功');
+      this.$store.commit('restoreForm', data);
     },
     getGzip() {
       const account = this.$store.state.account,
-          message = this.$store.state.message;
+        message = this.$store.state.message;
       if (Object.isEmpty(message))
         if (Object.isEmpty(account)) {
-          this.$message.error('没有任何内容需要压缩')
+          this.$message.error('没有任何内容需要压缩');
           return;
         }
-      const data = form2data(account, message)
+      const data = form2data(account, message);
       this.gzip = gzip.gzipEncode(JSON.stringify(data));
-      this.gzip2json(this.gzip)
-    }
-  }
-}
+      this.gzip2json(this.gzip);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
